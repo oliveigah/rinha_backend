2ac6f2adabfdccb51f199e4a46daa0eb999c1a7a
AkitaOnRails <fabioakita@gmail.com>
2023-09-11 15:49:45 -0300

    changing network mode from bridge to host to avoid the bridge mode cap under stress. but for some other reason the maximum inserts is still in the 38k inserts range. there is some other bottleneck in this implementation

--- config/runtime.exs
+++ config/runtime.exs
@@ -4,5 +4,5 @@ config :rinha_backend, RinhaRepo,
   database: "rinha_backend",
   username: "user",
   password: "pass",
-  hostname: "database",
+  hostname: "localhost",
   pool_size: System.fetch_env!("DB_CONNS") |> String.to_integer()
